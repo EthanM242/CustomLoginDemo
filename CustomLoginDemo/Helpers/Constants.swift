a279072f9a013657b80d8fb8885344d1e897bc17
Ethan M <ethanmui21@gmail.com>
2020-07-16 15:18:38 -0400

    hi

---
+++ CustomLoginDemo/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  CustomLoginDemo
+//
+//  Created by Ethan Mui on 2020-07-10.
+//  Copyright © 2019 Ethan Mui. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
